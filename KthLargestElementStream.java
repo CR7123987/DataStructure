ef23b0bef0418683c64bdd4cf8b130330c688028
CR7123987 <127672459+CR7123987@users.noreply.github.com>
2023-05-16 00:45:27 +0530

    Update KthLargestElementStream.java

--- KthLargestElementStream.java
+++ KthLargestElementStream.java
@@ -1,1 +1,9 @@
-
+//Design a class to efficiently find the Kth largest element in a stream of numbers.
+public class KthLargestElementStream{
+  private int[] array;
+  private PriorityQueue<Integer> queue;
+  private int size;
+   public KthLargestElementStream(int[] array,k){
+    
+   }
+}
