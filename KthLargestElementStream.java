7f219e20e7a97d0e708e1add8503c9a611d6ede6
CR7123987 <127672459+CR7123987@users.noreply.github.com>
2023-05-16 01:14:37 +0530

    Update KthLargestElementStream.java

--- KthLargestElementStream.java
+++ KthLargestElementStream.java
@@ -4,6 +4,27 @@ public class KthLargestElementStream{
   private PriorityQueue<Integer> queue;
   private int size;
    public KthLargestElementStream(int[] array,k){
-    
+      this.array=array;
+     this.size=k;
+     queue=new PriorityQueue<>();
+     for(int i=0;i<array.length;i++){
+        queue.add(array[i]);
+       if(queue.size()>size){
+         //removing the smallest,only k elemnts are left in queue and kth largest element is the next smallest
+        queue.poll();
+       }
+     }
    }
+  
+  public void add(int n){
+    queue.add(n);
+    if(queue.size()>size){
+      //removing the smallest,only k elemnts are left in queue and kth largest element is the next smallest
+      queue.poll();
+    }
+  }
+  
+  public int get(){
+    return queue.peek();
+  }
 }
